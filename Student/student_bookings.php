27bdaa1ada0ab0eb2d66fef8950b6c5e6c2a2405
hazeeqnajmuddin <hazeeqhaeqal@gmail.com>
2025-12-28 00:49:12 +0800

    Sidebar Update

--- Student/student_bookings.php
+++ Student/student_bookings.php
@@ -156,9 +156,6 @@ if (isset($_POST['cancel_booking'])) {
     <a href="student_parking_availability.php">
       <i class="fas fa-list"></i> Parking Availability
     </a>
-    <a href="student_my_parking.php">
-      <i class="fas fa-car-side"></i> My Parking
-    </a>
     </div>
     
 
